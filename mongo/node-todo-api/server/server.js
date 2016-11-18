276b1ba916e3a46a0d50e41823abe1567b0bb761
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-11-18 15:32:02 +0100

    Add GET /todos/:id

--- mongo/node-todo-api/server/server.js
+++ mongo/node-todo-api/server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +28,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
