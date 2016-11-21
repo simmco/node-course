1df4922f01a2d1704956a059cc9ce5e73bd39621
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-11-21 10:06:13 +0100

    Add DELETE /todos/:id route

--- mongo/node-todo-api/server/server.js
+++ mongo/node-todo-api/server/server.js
@@ -47,6 +47,22 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  var id = req.params.id;
+  //validate the id
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  //remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());  
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
