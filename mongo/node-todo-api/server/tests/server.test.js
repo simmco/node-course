cdd149cd99aceae0ce65de3e0b7e33bc39d30587
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-11-22 16:32:48 +0100

    Setup sepearate test and development envs

--- mongo/node-todo-api/server/tests/server.test.js
+++ mongo/node-todo-api/server/tests/server.test.js
@@ -161,7 +161,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
-        expect(res.body.todo.completedAt)toBeA('number');
+        expect(res.body.todo.completedAt).toBeA('number');
       })
       .end(done)
   });
@@ -180,7 +180,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
-        expect(res.body.todo.completedAt)toNotExist();
+        expect(res.body.todo.completedAt).toNotExist();
       })
       .end(done)
   });
